db5452caef4927fd6cac69e1d56b38e68b203d9d
Injamhossan <injamhossanmamun@gmail.com>
2025-08-13 23:42:17 +0600

    Problem-01 rechecked

--- Problem-01.js
+++ Problem-01.js
@@ -1,12 +1,17 @@
 function totalFine(fare) {
-    
-    if (typeof fare !== "number" || fare <= 0) {
+    if (typeof fare !== 'number' || fare <= 0) {
         return "Invalid";
     }
+    let surcharge = fare * 0.20;
+    let fine = fare + surcharge + 30;
 
-       let surcharge = fare * 0.20; 
-    let serviceCharge = 30;     
-    let total = fare + surcharge + serviceCharge;
-
-    return total;
+    return fine;
 }
+
+console.log(totalFine(200));  
+console.log(totalFine(0));  
+console.log(totalFine(50)); 
+console.log(totalFine(552));
+console.log(totalFine(-35));
+console.log(totalFine("65"));
+console.log(totalFine("Gorib tai ticket katinai"));
